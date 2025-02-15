dfcc8c049549a5776bcef7d290ce47c9bc643394
Nadejda Subeva <123803434+nadejdasubeva@users.noreply.github.com>
2025-02-15 10:43:56 +0200

    Add files via upload

--- ProjectMovieBookDB/Models/Genre.cs
+++ ProjectMovieBookDB/Models/Genre.cs
@@ -5,4 +5,5 @@ public class Genre
     public int Id { get; set; }
     public string Name { get; set; } = string.Empty;
     public List<Book> Books { get; set; } = new();
+    public List<Movie> Movies { get; set; } = new();
 }
