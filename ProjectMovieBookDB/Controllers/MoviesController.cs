9b0df3d623520f47bf8c30476e562446c3c888d7
Nadejda Subeva <123803434+nadejdasubeva@users.noreply.github.com>
2025-02-15 10:40:23 +0200

    Add files via upload

---
+++ ProjectMovieBookDB/Controllers/MoviesController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProjectMovieBookDB.Models;
+
+namespace ProjectMovieBookDB.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class MoviesController : ControllerBase
+{
+    private readonly BookCatalogContext _context;
+
+    public MoviesController(BookCatalogContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Movie>>> GetMovies()
+    {
+        return await _context.Movies.Include(m => m.Director).Include(m => m.Genres).ToListAsync();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Movie>> GetMovie(int id)
+    {
+        var movie = await _context.Movies.Include(m => m.Director).Include(m => m.Genres)
+                        .FirstOrDefaultAsync(m => m.Id == id);
+        if (movie == null)
+            return NotFound();
+
+        return movie;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Movie>> CreateMovie(Movie movie)
+    {
+        _context.Movies.Add(movie);
+        await _context.SaveChangesAsync();
+        return CreatedAtAction(nameof(GetMovie), new { id = movie.Id }, movie);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> UpdateMovie(int id, Movie movie)
+    {
+        if (id != movie.Id)
+            return BadRequest();
+
+        _context.Entry(movie).State = EntityState.Modified;
+        await _context.SaveChangesAsync();
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteMovie(int id)
+    {
+        var movie = await _context.Movies.FindAsync(id);
+        if (movie == null)
+            return NotFound();
+
+        _context.Movies.Remove(movie);
+        await _context.SaveChangesAsync();
+        return NoContent();
+    }
+}
