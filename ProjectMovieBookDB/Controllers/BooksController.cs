5784ae4b48290acd4ac21aa715b9320e45ca1df7
Lachezar Donchev <123803282+LachezarDonchev@users.noreply.github.com>
2025-02-15 09:47:33 +0200

    Add files via upload

---
+++ ProjectMovieBookDB/Controllers/BooksController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectMovieBookDB.Controllers;
+
+public class BooksController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
