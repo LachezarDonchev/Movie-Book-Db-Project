85c57928e1eba7a6a7d3cad599d15af014186edb
Nadejda Subeva <123803434+nadejdasubeva@users.noreply.github.com>
2025-02-18 20:40:00 +0200

    Add files via upload

---
+++ Models/BookCatalogContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace ProjectMovieBookDB.Models;
+
+public class BookCatalogContext : DbContext
+{
+    public DbSet<Book> Books { get; set; }
+    public DbSet<Movie> Movies { get; set; }
+    public DbSet<Author> Authors { get; set; }
+    public DbSet<Genre> Genres { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=BookCatalogDB;Trusted_Connection=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Book>()
+            .HasOne(b => b.Author)
+            .WithMany(a => a.Books)
+            .HasForeignKey(b => b.AuthorId);
+
+        modelBuilder.Entity<Book>()
+            .HasMany(b => b.Genres)
+            .WithMany(g => g.Books);
+    }
+}
